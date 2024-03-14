1af004b23da3561e27b1fea2eb7b575b505c794c
Yehor321 <120057177+Yehor321@users.noreply.github.com>
2024-03-14 23:21:18 +0200

    hi

--- js/loading.js
+++ js/loading.js
@@ -1,11 +1,11 @@
 document.body.onload = function () {
     setTimeout(function () {
         var preloader = document.getElementById('page-preloader');
-        var main = document.querySelector('main'); // Select the main element
+        var main = document.querySelector('main'); 
 
         if (!preloader.classList.contains('done')) {
             preloader.classList.add('done');
-            main.classList.add('done1'); // Add the done1 class to main
+            main.classList.add('done1'); 
         }
     }, 1000);
 }
--- js/script.js
+++ js/script.js
@@ -3,18 +3,13 @@ document.addEventListener('DOMContentLoaded', initGame);
 function initGame() {
     const gameBoard = document.getElementById('game-board');
     const cells = createGameBoard(gameBoard);
-    
-    // Add snake to the initial cell
     const initialCell = cells[0];
     initialCell.classList.add('snake');
-
-    // TODO: Add code for controlling the snake and updating the game state
 }
 
 function createGameBoard(gameBoard) {
     const cells = [];
 
-    // Create game board cells
     for (let i = 0; i < 10; i++) {
         for (let j = 0; j < 10; j++) {
             const cell = document.createElement('div');
@@ -26,3 +21,20 @@ function createGameBoard(gameBoard) {
 
     return cells;
 }
+
+
+let restart = document.querySelector(".restart");
+let scoreDisplay = document.querySelector(".score");
+
+let currentSnake = [2, 1, 0];
+let speed = 0.8;
+let width = 10;
+let currentIndex = 0;
+let appleIndex = 0;
+let direction = 1;
+let intervalTime = 0;
+let interval = 0;
+
+
+
+
